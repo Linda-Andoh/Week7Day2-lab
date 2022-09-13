1cd2ff11228105d84e2248a847be9c73201f5634
Linda <lexicon736@gmail.com>
2022-09-12 23:22:58 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
+//...................
+//Greetings
+
+// const express = require('express');
+
+// const app = express();
+
+// app.get('/greeting/:name', (req, res) => {
+//     res.json({
+//         greeting: `Hello ${req.params.name}, It's so great to see you!`
+//     });
+// });
+
+// app.listen(3000, () => {
+//     console.log('listening on port 3000!');
+// });
+
+
+//......................../
+//Tip Calculator//
+
 const express = require('express');
 
 const app = express();
 
-app.get('/greeting/:name', (req, res) => {
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    let tip = req.params.total * req.params.tipPercentage / 100;
     res.json({
-        greeting: `Hello ${req.params.name}, It's so great to see you!`
+        tip: `Hey Your tip is ${tip}` 
     });
 });
+
 
 app.listen(3000, () => {
     console.log('listening on port 3000!');
